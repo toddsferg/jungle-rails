ebf10e62c750cbe836326057decdcdb31f66f086
Todd Ferguson <toddsferg@gmail.com>
2016-11-11 22:03:47 +0000

    Add user authentication funtionality
    Added user authentication functionality. Now a user can sign in to the site.

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,15 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  # signups
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+
+  #sessions
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
